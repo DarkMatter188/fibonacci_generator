0975bc796a8d4b9c8913f4e24c7961b396ecb459
DarkMatter188 <61989754+DarkMatter188@users.noreply.github.com>
2020-04-08 13:39:51 +0530

    Add files via upload

---
+++ Challenge2.py
@@ -0,0 +1,13 @@
+# generator to print Fibonacci numbers
+def genfibo(n):
+    a,b=0,1
+    while(n!=0):
+        yield a
+        a,b=b,a+b
+        n-=1
+f=genfibo(7)
+for i in f:
+    print(i)
+
+
+
